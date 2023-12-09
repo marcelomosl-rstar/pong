a8fddd4a492d1e1c08e7aed6808124b372ce022b
marcelomosl-rstar <marcelo.mosl12@gmail.com>
2023-12-09 20:10:22 -0300

    feat: pong draws and keypressEvents

--- main.lua
+++ main.lua
@@ -13,6 +13,10 @@ function love.load()
 
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
+    retroSmallFont = love.graphics.newFont('font.ttf', 8)
+
+    love.graphics.setFont(retroSmallFont)
+
 
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -31,11 +35,18 @@ function love.load()
     -- })
 end
 
+local enterPressed = false
+
+
 function love.keypressed(key)
     -- keys can be accessed by string name
     if key == 'escape' then
         -- function LÖVE gives us to terminate application
         love.event.quit()
+    end
+
+    if key == 'return' then  -- 'return' is the correct name for the enter key
+        enterPressed = not enterPressed
     end
 end
 
@@ -48,12 +59,35 @@ function love.draw()
 
     push:apply("start")
 
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+
+    if enterPressed then
+        love.graphics.printf("Marcelo Mosl", 
+            0, 
+            VIRTUAL_HEIGHT / 2 - 20, 
+            VIRTUAL_WIDTH, 
+            "center"
+        )
+    end
+
 
     love.graphics.printf("Pong Virtual Dimensions", 
     0, 
-    VIRTUAL_HEIGHT / 2 -6, 
+    VIRTUAL_HEIGHT / 2 -120, 
     VIRTUAL_WIDTH, 
     "center")
+
+
+    -- settings order mode, positionX, positionY, width, height
+    -- LEFT
+    love.graphics.rectangle("fill" , 5, 30, 5, 20)
+
+
+    --RIGTH
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+
+    -- render ball (center)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
     push:apply("end")
 end
