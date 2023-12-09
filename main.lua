5ca1a0f78d05bf22ef4e6aeb7dec6e12c1c9e071
marcelomosl-rstar <marcelo.mosl12@gmail.com>
2023-12-09 19:39:47 -0300

    feat: Pong Virtual Dimensions

--- main.lua
+++ main.lua
@@ -1,20 +1,59 @@
-WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 600
+WINDOW_WIDTH = 1270 
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+push = require 'push'
+
 
 
 
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
     })
+
+
+
+
+    -- love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    --     fullscreen = false,
+    --     resizable = false,
+    --     vsync = true
+    -- })
 end
 
+function love.keypressed(key)
+    -- keys can be accessed by string name
+    if key == 'escape' then
+        -- function LÖVE gives us to terminate application
+        love.event.quit()
+    end
+end
+
+
+
+
+
 function love.draw()
-    love.graphics.printf("Pong  Initial", 
+
+
+    push:apply("start")
+
+
+    love.graphics.printf("Pong Virtual Dimensions", 
     0, 
-    WINDOW_HEIGHT / 2 -6, 
-    WINDOW_WIDTH, 
+    VIRTUAL_HEIGHT / 2 -6, 
+    VIRTUAL_WIDTH, 
     "center")
+
+    push:apply("end")
 end
